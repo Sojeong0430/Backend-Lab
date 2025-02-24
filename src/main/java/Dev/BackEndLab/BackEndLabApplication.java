8d18fe342669ac57828abb9b12e25333031aaee1
Sojeong0430 <dlthwjd1629@gmail.com>
2025-02-24 13:04:00 +0900

    프로젝트 세팅

---
+++ src/main/java/Dev/BackEndLab/BackEndLabApplication.java
@@ -0,0 +1,13 @@
+package Dev.BackEndLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndLabApplication.class, args);
+	}
+
+}
